5a763decf70b074f5a8aff75bdcfea638f3e5592
Sayo-Shiozuka <sayosf1026@gmail.com>
2021-02-04 09:58:07 +0000

    [finish]3-8課題２

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render template: "books/index"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed"
+      redirect_to books_path(book.id)
+    else
+      render books_path(book.id)
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(@book)
+    else
+      render template: "books/edit"
+    end
+  end
+
+  private
+  def book_params
+  params.require(:book).permit(:title, :body)
+  end
+end
